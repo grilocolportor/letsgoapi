8888e94cc40ed6dc0675d86b3bb80a2641997259
Venâncio <grilocolportor@yahoo.com.br>
2018-10-25 16:05:25 -0300

    add queries

--- user/persistence/crud.py
+++ user/persistence/crud.py
@@ -1,12 +1,88 @@
 from __future__ import print_function
 
 import mysql.connector
+import json
 from mysql.connector import errorcode
 
 DB_NAME = 'letsgo'
 TB_NAME = 'USER'
 
-def insert():
+def queryByParam(user):
+    """ get all row by param """
+    try:
+        conn = mysql.connector.connect(host='localhost', 
+                                       database='letsgo', 
+                                       user='root', 
+                                       password='root')
+        
+        if conn.is_connected():
+            print("Connected to MySql Databae: {} \n".format(DB_NAME), end='')
+            cursor = conn.cursor()
+
+            param = ""
+
+            if  user['id']:
+                param = param + " and ID = '" + user['id'] + "' "
+            if  user['nome']:
+                param = param + " and NAME = '" +user['nome'] + "' "
+            if  user['senha']:
+                param = param + " and PASSWORD = '" + user['senha'] + "' "
+            if  user['phone']:
+                param = param + " and PHONE = '" + user['phone'] + "' "
+
+            sql = (" SELECT * FROM USER where 1=1 " + param)
+
+            print(sql)
+
+            cursor.execute(sql)
+
+            row = cursor.fetchone()
+
+            while row is not None:
+                print('\n{}'.format(row), end='')
+                row = cursor.fetchone()
+
+            cursor.close()
+
+    except ValueError as err:
+        print(err)
+    
+    finally:
+        conn.close()
+        print('\n Database desconnected \n')
+
+def queryAll():
+    """ get all row from table """
+    try:
+        conn = mysql.connector.connect(host='localhost', 
+                                       database='letsgo', 
+                                       user='root', 
+                                       password='root')
+        
+        if conn.is_connected():
+            print("Connected to MySql Databae: {} \n".format(DB_NAME), end='')
+            cursor = conn.cursor()
+
+            sql = (" SELECT * FROM USER ")
+
+            cursor.execute(sql)
+
+            row = cursor.fetchone()
+
+            while row is not None:
+                print('\n{}'.format(row), end='')
+                row = cursor.fetchone()
+
+            cursor.close()
+
+    except ValueError as err:
+        print(err)
+    
+    finally:
+        conn.close()
+        print('\n Database desconnected \n')
+
+def insert(user):
     """ Insert new user """
     try:
         conn = mysql.connector.connect(host='localhost', 
@@ -23,24 +99,19 @@ def insert():
                 " VALUES "
                 " (%(nome)s, %(senha)s, %(phone)s, %(image)s) ")
 
-            user = {
-                'nome': 'teste1',
-                'senha': 'ttt',
-                'phone': '(81)123456789',
-                'image': '/the/path/of/image'
-            }
-
             cursor.execute(sql, user)
 
             conn.commit()
 
             cursor.close()
 
-    except Error as err:
+    except ValueError as err:
         print(err)
     
     finally:
         conn.close()
+        print('Database desconnected \n')
 
 if __name__=='__main__':
-    insert()
+    user = json.loads('{"id": "", "nome": "teste1", "senha": "", "phone": "",  "image": ""}')
+    queryByParam(user)
