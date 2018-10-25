5d586e99c41438d55c7d26ee84750746651d00e6
Venâncio <grilocolportor@yahoo.com.br>
2018-10-25 16:32:39 -0300

    add update

--- user/persistence/crud.py
+++ user/persistence/crud.py
@@ -7,6 +7,65 @@ from mysql.connector import errorcode
 DB_NAME = 'letsgo'
 TB_NAME = 'USER'
 
+def update(user):
+    """update item by param"""
+    try:
+        conn = mysql.connector.connect(host='localhost', 
+                                       database='letsgo', 
+                                       user='root', 
+                                       password='root')
+        
+        if conn.is_connected():
+            print("Connected to MySql Databae: {} \n".format(DB_NAME), end='')
+            cursor = conn.cursor()
+
+            param = mountSql(user)
+
+            if not param:
+                sql = (" UPDATE USER(NAME = %(nome)s, PASSWORD=%(senha)s, PHONE=%(phone)s) where 1=1 " + param + ";")
+                print(sql)
+                cursor.execute(sql)
+                conn.commit()
+
+            cursor.close()
+
+    except ValueError as err:
+        print(err)
+    
+    finally:
+        conn.close()
+        print('\n Database desconnected \n')
+
+def delete(user):
+    """delete item by param"""
+    try:
+        conn = mysql.connector.connect(host='localhost', 
+                                       database='letsgo', 
+                                       user='root', 
+                                       password='root')
+        
+        if conn.is_connected():
+            print("Connected to MySql Databae: {} \n".format(DB_NAME), end='')
+            cursor = conn.cursor()
+
+            param = mountSql(user)
+
+            if not param:
+                sql = (" DELETE  FROM USER where 1=1 " + param + ";")
+                print(sql)
+                cursor.execute(sql)
+                conn.commit()
+
+            cursor.close()
+
+    except ValueError as err:
+        print(err)
+    
+    finally:
+        conn.close()
+        print('\n Database desconnected \n')
+
+
 def queryByParam(user):
     """ get all row by param """
     try:
@@ -19,18 +78,9 @@ def queryByParam(user):
             print("Connected to MySql Databae: {} \n".format(DB_NAME), end='')
             cursor = conn.cursor()
 
-            param = ""
+            param = mountSql(user)
 
-            if  user['id']:
-                param = param + " and ID = '" + user['id'] + "' "
-            if  user['nome']:
-                param = param + " and NAME = '" +user['nome'] + "' "
-            if  user['senha']:
-                param = param + " and PASSWORD = '" + user['senha'] + "' "
-            if  user['phone']:
-                param = param + " and PHONE = '" + user['phone'] + "' "
-
-            sql = (" SELECT * FROM USER where 1=1 " + param)
+            sql = (" SELECT * FROM USER where 1=1 " + param + ";")
 
             print(sql)
 
@@ -63,7 +113,7 @@ def queryAll():
             print("Connected to MySql Databae: {} \n".format(DB_NAME), end='')
             cursor = conn.cursor()
 
-            sql = (" SELECT * FROM USER ")
+            sql = (" SELECT * FROM USER;")
 
             cursor.execute(sql)
 
@@ -112,6 +162,20 @@ def insert(user):
         conn.close()
         print('Database desconnected \n')
 
+def mountSql(user):
+    param = ""
+
+    if  user['id']:
+        param = param + " and ID = " + user['id'] + " "
+    if  user['nome']:
+        param = param + " and NAME = '" +user['nome'] + "' "
+    if  user['senha']:
+        param = param + " and PASSWORD = '" + user['senha'] + "' "
+    if  user['phone']:
+        param = param + " and PHONE = '" + user['phone'] + "' "
+    
+    return param
+
 if __name__=='__main__':
-    user = json.loads('{"id": "", "nome": "teste1", "senha": "", "phone": "",  "image": ""}')
-    queryByParam(user)
+    user = json.loads('{"id": "2", "nome": "", "senha": "", "phone": "",  "image": ""}')
+    queryAll()
